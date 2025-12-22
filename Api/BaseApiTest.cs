211a126cd88c1ca0ca0388f5b138f6e15d0e6470
tupdikar <tupdikar.s@gmail.com>
2025-12-22 10:46:27 +0530

    Api Test Project First Setup

---
+++ Api/BaseApiTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Playwright;
+using NUnit.Framework;
+
+namespace PlaywrightApiTest
+{
+    public class BaseApiTest
+    {
+        protected IAPIRequestContext Request;
+
+        [OneTimeSetUp]
+        public async Task Setup()
+        {
+            var playwright = await Playwright.CreateAsync();
+
+            Request = await playwright.APIRequest.NewContextAsync(new()
+            {
+                BaseURL = "https://reqres.in",
+                ExtraHTTPHeaders = new Dictionary<string, string>
+                {
+                    { "Accept", "application/json" }
+                }
+            });
+        }
+
+        [OneTimeTearDown]
+        public async Task TearDown()
+        {
+            await Request.DisposeAsync();
+        }
+    }
+}
---
+++ Api/UserApiTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using System.Text.Json;
+
+namespace PlaywrightApiTest.Api
+{
+
+    [TestFixture]
+    public class UserApiTests : BaseApiTest
+    {
+        [Test]
+        public async Task GetUsers_ShouldReturn200()
+        {
+            var response = await Request.GetAsync("/api/users?page=2");
+
+            Assert.That(response.Status, Is.EqualTo(200));
+
+            var responseBody = await response.TextAsync();
+            Assert.That(responseBody, Does.Contain("email"));
+        }
+    }
+
+}
